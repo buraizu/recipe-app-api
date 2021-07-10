56ee71bb48c6e52a3f2046fa435f887115f5df4e
Bryce Eadie <bryce.eadie@gmail.com>
2021-07-10 14:47:07 -0700

    Adds feature for listing ingredients

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -51,3 +51,12 @@ class ModelTests(TestCase):
             name='Vegan'
         )
 
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
+
